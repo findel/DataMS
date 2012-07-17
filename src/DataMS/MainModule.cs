4afb29c2ad6775cd3f6327c8312f6f2b1740f343
Phil Baines <phil.baines@wubbleyew.com>
2012-07-17 15:25:33 +0100

    Add Main Module - and Hello World

---
+++ src/DataMS/MainModule.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Nancy;
+
+namespace DataMS
+{
+	public class MainModule : NancyModule
+	{
+	    public MainModule() : base("/_DataMS")
+	    {
+	        // Capture /_DataMS/
+	        Get["/"] = parameters => {
+	            return "Hello DataMS application!";
+	        };
+	    }
+	}
+}
